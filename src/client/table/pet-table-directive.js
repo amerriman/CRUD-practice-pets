74ccc9cee091dd4ec2ab148107bcc71fd18badd2
amerriman <amm434@gmail.com>
2016-01-08 15:47:24 -0700

    done

--- src/client/table/pet-table-directive.js
+++ src/client/table/pet-table-directive.js
@@ -4,16 +4,40 @@ app.directive('petTable', function(){
     templateUrl: '/table/pet-table.html',
     controller: ['$scope', '$http', 'httpFactory', function ($scope, $http, httpFactory){
 
+
       getPets = function(url){
         httpFactory.get(url)
         .then(function(response){
           $scope.pets = response.data;
-          console.log($scope.pets, "scope pets");
+          // console.log($scope.pets, "scope pets");
         });
       };
       getPets('api/pets');
 
+      $scope.deletePet = function(id) {
+        httpFactory.delete('api/pet/' + id)
+        .then(function(response){
+          console.log(response, "DELETE RESPONSE");
+          getPets('api/pets');
+        });
+      };
+
+      $scope.editPet = function(name, type, age, id){
+        var payload = {
+          'name': name,
+          'type': type,
+          'age': age
+        };
+        httpFactory.put('api/pet/'+id, payload)
+        .then(function(response){
+          console.log(response.data, "edit response");
+          getPets('api/pets');
+        });
+      };
+
+
+
 
     }]
-  }
-})
+  };
+});
